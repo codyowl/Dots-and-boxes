c0b9b90fa1303280c6134d9362afe991853558a6
codyowl <codyowl@gmail.com>
2020-08-17 23:26:50 +0530

    user model added :heavy_plus_sign:

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,13 +1,13 @@
 import datetime
 from sqlalchemy import Column, Integer, DateTime
-from sqlalchemy.ext.declarative import as_declarative
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 
 @as_declarative()
 class Base(object):
     id = Column(Integer, primary_key=True)
     created_date = Column(DateTime, default=datetime.datetime.utcnow)
-    
+
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
---
+++ backend/app/models/user.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.models.base import Base
+
+class User(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean(), default=True)
+    is_superuser = Column(Boolean(), default=False)
+    games_played = relationship("Game", back_populates="gamesplayed")
+    level = Column(Integer,default=0)
+    win_count = Column(Integer,default=0)
+    total_games = Column(Integer, default=0)
+    comments_received = Column(Integer, default=0)
+
+    # helper function to get loss count
+    def get_loss_count(self):
+        return total_games - win_count
+
+
+    
+
+
+
